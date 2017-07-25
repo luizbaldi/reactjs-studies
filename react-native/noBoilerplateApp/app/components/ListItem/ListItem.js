525ecab83f1a6f7d8dda3775dc4be734c5d9c959
Luiz Baldi <baldilp@gmail.com>
2017-07-24 22:39:20 -0300

    adding listItem component to contacts screen

--- react-native/noBoilerplateApp/app/components/ListItem/ListItem.js
+++ react-native/noBoilerplateApp/app/components/ListItem/ListItem.js
@@ -2,26 +2,24 @@ import React from 'react';
 import {
     View,
     Text,
-    Image,
     TouchableHighlight,
+    Image,
     Platform
 } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 import styles, { CHEVRON_SIZE } from './styles';
 import { capitalizeFirstLetter } from '../../helpers/string';
 import colors from '../../config/colors';
 
 const ListItem = ({ contact, onPress }) => {
-    const iconName = Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward';
     const name = `${capitalizeFirstLetter(contact.name.first)} ${capitalizeFirstLetter(contact.name.last)}`;
     return (
         <TouchableHighlight
             onPress={onPress}
             underlayColor={colors.rowUnderlay}
         >
-            <View style={styles.row}>
-                <Image
+            <View>
+                <Image 
                     style={styles.avatar}
                     source={{ uri: contact.picture.thumbnail }}
                 />
@@ -30,16 +28,11 @@ const ListItem = ({ contact, onPress }) => {
                     <Text style={styles.email}>{contact.email}</Text>
                 </View>
                 <View style={styles.chevronContainer}>
-                    <Icon
-                        name={iconName}
-                        size={CHEVRON_SIZE}
-                        style={styles.chevron}
-                        color={colors.subtleText}
-                    />
+                    <Text>Arrow</Text>
                 </View>
             </View>
         </TouchableHighlight>
-    )
+    );
 };
 
 export default ListItem;
--- react-native/noBoilerplateApp/app/screens/Contacts.js
+++ react-native/noBoilerplateApp/app/screens/Contacts.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+
 import { contacts } from '../config/data';
 import colors from '../config/colors';
+import { ListItem } from '../components/ListItem';
 
 class Contacts extends Component {
+	super(props) {
+		constructor(props);
+	}
 	render() {
 		return (
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				<Text>Contacts Screen</Text>
-				<FlatList
-					style={{ backgroundColor: colors.background}}
-					data={contacts}
-  					renderItem={({item}) => <View><Text>{item.email}</Text></View>}
-					keyExtractor={(item) => item.email}
-				/>
-			</View>
+			<FlatList
+				style={{ backgroundColor: colors.background}}
+				data={contacts}
+				renderItem={({ item }) => 
+					<ListItem contact={item} /> 
+				}
+				keyExtractor={(item) => item.email}
+			/>
 		);
 	}
 };
