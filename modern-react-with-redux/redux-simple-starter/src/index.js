55e205b90208237e5ff74393c86ce23d781d0053
Luiz Baldi <baldilp@gmail.com>
2017-11-05 21:18:29 -0200

    created folder for Modern React With Redux course with a simple boilerplate from the course

---
+++ modern-react-with-redux/redux-simple-starter/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+    <div>Potatoe!</div>
+)
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
