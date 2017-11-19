fa6b1fc0766d3856932f9587e80dc4b6dfd562a1
Luiz Baldi <baldilp@gmail.com>
2017-11-19 21:32:29 -0200

    Simply add lodash omit method for handling state better

--- modern-react-with-redux/redux-form-app/src/reducers/PostsReducer.js
+++ modern-react-with-redux/redux-form-app/src/reducers/PostsReducer.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { 
+  FETCH_POSTS,
+  FETCH_POST,
+  DELETE_POST
+} from '../actions';
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -8,6 +12,8 @@ export default (state = {}, action) => {
     case FETCH_POST:
       const { data } = action.payload;
       return { ...state, [data.id]: data };
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
