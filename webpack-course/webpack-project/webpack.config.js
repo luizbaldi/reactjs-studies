467a5263fc0cbb252567a39c8fddd828b6aeb242
Luiz Baldi <baldilp@gmail.com>
2018-01-10 18:22:53 -0200

    Add webpack project folder with stephen grinder boilerplate Boilerplate url can be found here: https://github.com/stephengrider/webpackproject

---
+++ webpack-course/webpack-project/webpack.config.js
@@ -0,0 +1,10 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
