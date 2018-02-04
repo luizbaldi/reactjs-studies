54a10b45ed35615506ab584dfe9ed41b60e6343b
Luiz Baldi <baldilp@gmail.com>
2018-02-04 21:59:44 -0200

    Remove unecessary files from boilerplate and create simple header component

---
+++ the-complete-react-native-and-redux-course/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Header from './components/Header';
+
+const App = () => (
+  <View>
+    <Header/>
+    <Text>Potatoes!</Text>
+  </View>
+);
+
+export default App;
---
+++ the-complete-react-native-and-redux-course/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => (
+  <Text>Albums App</Text>
+);
+
+export default Header;
