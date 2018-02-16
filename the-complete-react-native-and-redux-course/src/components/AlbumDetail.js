727bbd8bb86522d0403f3945511e7c4395b64a0d
Luiz Baldi <baldilp@gmail.com>
2018-02-16 21:00:25 -0200

    Section 8 progress commit - Just start to study flex layout on react native more deeply

--- the-complete-react-native-and-redux-course/src/components/AlbumDetail.js
+++ the-complete-react-native-and-redux-course/src/components/AlbumDetail.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ album }) => (
-  <Card>
-    <CardSection>
-      <Text>{album.title}</Text>
-    </CardSection>
-  </Card>
-);
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  return (
+    <Card>
+      <CardSection>
+        <View>
+          <Image 
+            source={{ uri: thumbnail_image }}
+            style={style.thumbnail}
+          />
+        </View>
+        <View style={style.header}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
+  );
+}
+
+const style = {
+  header: {
+    justifyContent: 'space-around'
+  },
+  thumbnail: {
+    height: 60,
+    width: 60
+  }
+}
 
 export default AlbumDetail;
