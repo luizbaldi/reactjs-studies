fd51be35d727c63b8659da39e4c929f0974556a1
Baldi <baldilp@gmail.com>
2018-11-05 11:39:01 -0200

    add some simple react hook example to joke with useEffect and stuff

--- hooks/effect-hook-examples/src/App.js
+++ hooks/effect-hook-examples/src/App.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [secondaryCount, setSecondaryCounter] = useState(0)
+  const [input, setInputValue] = useState('')
+
+  useEffect(() => {
+    /* notice that every change on hook state will fire useEffect */
+    /* 'Effect fired' log is called on first render (like componentDidMount) */
+    console.log('Effect fired')
+
+    return () => {
+      /* notice that cleanup effect is called only after first re-render */
+      console.log('Cleanup effect fired')
+    }
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Simple durp examples with react hooks
+        </p>
+        <div className="counter-container">
+          <div className="counter">
+            <p>Primary Counter: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+          </div>
+          <div className="counter">
+            <p>Secondary Counter: {secondaryCount}</p>
+            <button onClick={() => setSecondaryCounter(secondaryCount + 1)}>Increment</button>
+          </div>
+        </div>
+        <p>
+          Controlled input example. You typed: {input}
+        </p>
+        <input
+          type="text"
+          value={input}
+          placeholder='Type random things here'
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
