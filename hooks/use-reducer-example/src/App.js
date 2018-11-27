8f172bd9b5a5457493c93fd64e1d4360c43fedfe
Baldi <baldilp@gmail.com>
2018-11-26 23:48:19 -0200

    add deadly simple example of useReducer just to see how it works

--- hooks/use-reducer-example/src/App.js
+++ hooks/use-reducer-example/src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useReducer } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const initialState = {
+  counter: 0
+}
+
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case 'increment':
+      return { counter: state.counter + 1 }
+    case 'decrement':
+      return { counter: state.counter - 1 }
+    case 'clear':
+      return { counter: 0 }
+    default:
+      return state
   }
 }
 
-export default App;
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div>
+        Potatoe counter with useReducer
+        <p><i>(total overkill but it's just for study purposes so yea why not)</i></p>
+        <hr className="divider" />
+        <p>Number of potatoes: {state.counter}</p>
+        <button className='button' onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+        <button className='button' onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+        <button className='button' onClick={() => dispatch({ type: 'clear' })}>Clear</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
