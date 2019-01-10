f3cf176c965cbfcb30a388c22c8026a4b149f753
Baldi <baldilp@gmail.com>
2019-01-09 22:44:37 -0200

    add async routing with react-router-dom and suspense

--- suspense/async-router/src/App.js
+++ suspense/async-router/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component, lazy, Suspense } from 'react';
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
+import { Link, Route, BrowserRouter, Switch } from 'react-router-dom';
 
 /* components */
 import Fallback from './components/Fallback'
@@ -10,21 +10,39 @@ const Fun = lazy(() => import('./components/Fun'))
 const Nothing = lazy(() => import('./components/Nothing'))
 const Potatoe = lazy(() => import('./components/Potatoe'))
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Suspense fallback={<Fallback />}>
-            <Fun />
-            <Nothing />
-            <Potatoe />
-          </Suspense>
-        </header>
+const App = () => (
+  <BrowserRouter>
+    <>
+      <header className='app-header'>
+        <nav>
+          <Link className='app-link' to='/'>Home</Link>
+          <Link className='app-link' to='/fun'>Fun</Link>
+          <Link className='app-link' to='/nothing'>Nothing</Link>
+          <Link className='app-link' to='/potatoe'>Potatoe</Link>
+        </nav>
+      </header>
+      <div className='content'>
+        <Suspense fallback={Fallback}>
+          <Switch>
+            <Route exact path='/'>
+              <span>
+                This is an async react-router with Suspense example :)
+              </span>
+            </Route>
+            <Route path='/fun'>
+              <Fun />
+            </Route>
+            <Route path='/nothing'>
+              <Nothing />
+            </Route>
+            <Route path='/potatoe'>
+              <Potatoe />
+            </Route>
+          </Switch>
+        </Suspense>
       </div>
-    );
-  }
-}
+    </>
+  </BrowserRouter>
+)
 
 export default App;
--- suspense/async-router/src/components/Nothing.jsx
+++ suspense/async-router/src/components/Nothing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Fun = () => (
-  <span>This is a Nothing (?)</span>
+  <span>This is Nothing (?)</span>
 )
 
 export default Fun
