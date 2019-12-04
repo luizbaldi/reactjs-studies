8243cd57be77eeb21fdf94d129732e0de3cb3762
Luiz Baldi <baldilp@gmail.com>
2019-12-03 23:17:45 -0300

    Improve navigation structure and create a basic Home screen

--- typescript/finance-organizer/src/screens/Home/Home.tsx
+++ typescript/finance-organizer/src/screens/Home/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text } from 'react-native';
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const Home = () => (
   <StyledContainer>
-    <Text>Home Screen</Text>
+    <StyledHeader>
+      <StyledTitle>Year 2019</StyledTitle>
+      <StyledRightIcon>
+        <Icon name='setting' />
+      </StyledRightIcon>
+    </StyledHeader>
+    <StyledContent />
   </StyledContainer>
 );
 
@@ -12,9 +18,31 @@ Home.navigationOptions = {
   header: null
 };
 
-const StyledContainer = styled.ScrollView`
+const StyledContainer = styled.SafeAreaView`
   flex: 1;
   height: 200px;
+`;
+
+const StyledTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const StyledHeader = styled.View`
+  padding: 16px;
+  justify-content: center;
+  border-bottom-width: 1px;
+  border-bottom-color: black;
+`;
+
+const StyledRightIcon = styled.TouchableOpacity`
+  position: absolute;
+  right: 18px;
+`;
+
+const StyledContent = styled.View`
+  padding: 16px;
 `;
 
 export default Home;
