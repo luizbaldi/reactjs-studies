f24e6f03d1d11f89e5fc39aa31d61fc30e1a4669
Luiz Baldi <baldilp@gmail.com>
2020-01-12 20:11:11 -0300

    [finance-organizer] Update splash screen and icon

--- typescript/finance-organizer/src/App.tsx
+++ typescript/finance-organizer/src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import styled from 'styled-components/native';
+import SplashScreen from 'react-native-splash-screen';
 
 import Routes from './config/routes';
 
-const App = () => (
-  <StyledContainer>
-    <StatusBar barStyle='dark-content' />
-    <Routes />
-  </StyledContainer>
-);
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <StyledContainer>
+      <StatusBar barStyle='dark-content' />
+      <Routes />
+    </StyledContainer>
+  );
+};
 
 const StyledContainer = styled.SafeAreaView`
   flex: 1;
